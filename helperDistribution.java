300d3fcf3cfeb4c72fc1955b40126f6c6f9ca1ea
bopzz <ntm2972003@gmail.com>
2024-07-08 09:23:34 +0700

    added helperDistribution

---
+++ helperDistribution.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class helperDistribution {
+    public static int costNormalDist(int mean, int sd){
+        Random random = new Random();
+        double gaussian = random.nextGaussian();
+        double cost = mean + sd * gaussian;
+        return (int) Math.round(cost);
+    }
+
+    public static int costExponentialDist(double lambda) {
+        Random random = new Random();
+        double uniform = random.nextDouble();
+        double cost = Math.log(1 - uniform) / (-lambda);
+        return (int) Math.round(cost);
+    }
+}
